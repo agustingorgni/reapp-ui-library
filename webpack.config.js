ea15d978470033e5e76aac3ac488dbc47e52a899
agustingorgni <agustingorgni25@gmail.com>
2022-09-30 17:39:13 -0300

    Version upgraded@

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,9 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    entry: {
-        './src/button/index': './src/button/index.jsx',
-        './src/label/index': './src/label/index.jsx',
-    },
+    entry: path.resolve(__dirname, './src/index.jsx'),
     output: {
-        filename: '[name].js',
+        filename: 'index.js',
         path: path.resolve(__dirname, './dist'),
         libraryTarget: 'umd',
         umdNamedDefine: true,
