b6bed1719d2037b43433c4d426b6cff01da02ca3
agustingorgni <agustingorgni25@gmail.com>
2022-09-30 18:08:38 -0300

    Storybook added

--- src/button/index.jsx
+++ src/button/index.jsx
@@ -2,18 +2,41 @@ import React from 'react';
 import Styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Outlined = Styled.button`
-    padding: 16px;
-`;
+const Button = ({ label, onClick, size, color, ...props }) => {
+    const Large = Styled.button`
+        background-color: ${color};
+        padding: 16px;
+        border: none;
+        border-radius: 8px;
+    `;
 
-const Button = ({ label, onClick, ...props }) => {
-    let StyledButton = Outlined;
+    const Small = Styled.button`
+        background-color: ${color};
+        padding: 8px;
+        border: none;
+        border-radius: 8px;
+    `;
+
+    let StyledButton;
+    switch (size) {
+        case 'large':
+            StyledButton = Large;
+            break;
+        default:
+            StyledButton = Small;
+            break;
+    }
     return <StyledButton onClick={onClick} {...props}>{label}</StyledButton>
 };
 
 Button.propTypes = {
     label: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
+    size: PropTypes.oneOf(['large', 'small', 'medium']),
+};
+
+Button.defaultProps = {
+    size: 'small',
 };
 
 export { Button };
